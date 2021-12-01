943f8547e809bee578fb35dd0c49366483db0413
Lucs Lisboa <lucas@pop-os.localdomain>
2021-12-01 01:18:26 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const inputMeme = document.getElementById('text-input');
+const inputImg = document.getElementById('meme-insert');
 const paragrafo = document.getElementById('meme-text');
+const quadro = document.getElementById('meme-image');
 
 function mudaParagrafo() {
   paragrafo.innerText = inputMeme.value;
 }
 
+function memeFundo() {
+  const imagem = inputImg.files[0];
+  quadro.src = URL.createObjectURL(imagem);
+  paragrafo.classList.add('meme-texto');
+}
+
+inputImg.addEventListener('change', memeFundo);
 inputMeme.addEventListener('keyup', mudaParagrafo);
