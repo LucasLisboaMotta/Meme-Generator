6701d041f50566ba4e70aca5406d9c98d5061b17
Lucs Lisboa <lucas@pop-os.localdomain>
2021-12-01 02:19:45 -0300

    requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const inputMeme = document.getElementById('text-input');
 const inputImg = document.getElementById('meme-insert');
 const paragrafo = document.getElementById('meme-text');
 const quadro = document.getElementById('meme-image');
+const fogo = document.getElementById('fire');
+const agua = document.getElementById('water');
+const terra = document.getElementById('earth');
+const container = document.getElementById('meme-image-container');
 
 function mudaParagrafo() {
   paragrafo.innerText = inputMeme.value;
@@ -12,5 +16,20 @@ function memeFundo() {
   quadro.src = URL.createObjectURL(imagem);
 }
 
+function mudarFogo() {
+  container.className = 'fogo';
+}
+
+function mudarAgua() {
+  container.className = 'agua';
+}
+
+function mudarTerra() {
+  container.className = 'terra';
+}
+
+fogo.addEventListener('click', mudarFogo);
+agua.addEventListener('click', mudarAgua);
+terra.addEventListener('click', mudarTerra);
 inputImg.addEventListener('change', memeFundo);
 inputMeme.addEventListener('keyup', mudaParagrafo);
